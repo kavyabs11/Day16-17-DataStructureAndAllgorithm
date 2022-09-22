6e9303f3ce64e468ed595f72fc93fb3ff0a9c058
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-22 08:31:16 -0700

    Print the array in a sorted manner using insertion sort

--- Algorithm/Algo.cs
+++ Algorithm/Algo.cs
@@ -8,39 +8,30 @@ namespace Algorithm
 {
     internal class Algo
     {
-        public static void sortAndCall(String sentence, String find)         //find specifies the word to be searched
+        public static void insertionSort(String SENTENCE)
         {
-            String[] words = sentence.Split(' ');          //storing the string in an array of strings
-            Array.Sort(words);
-            Algo.search(words, find);                    //calling the searching function
-        }
-        public static void search(String[] sen, String find)         //Binary Search
-        {
-            int min = 0;
-            int max = sen.Length - 1;
-            int flag = 0;
-            while (min <= max)
+            String[] sentence = SENTENCE.Split(' ');
+            for (int x = 1; x < (sentence.Length) - 1; x++)
             {
-                int mid = (min + max) / 2;
-                if (sen[mid].CompareTo(find) == 0)
+                int i = x;
+                String t = sentence[x];
+                while (sentence[i - 1].CompareTo(t) > 0)
                 {
-                    flag = 1;
-                    Console.WriteLine("Found at " + mid + " position ");
-                    break;
+                    sentence[i] = sentence[i - 1];
+                    i--;
+                    if (i == 0)
+                    {
+                        break;
+                    }
                 }
-                else if (sen[mid].CompareTo(find) > 0)
-                {
-                    max = mid - 01;
-                }
-                else
-                {
-                    min = mid + 1;
-                }
+                sentence[i] = t;
             }
-            if (flag == 0)
+            Console.WriteLine();
+            for (int x = 0; x < sentence.Length; x++)
             {
-                Console.WriteLine("Not found");
+                Console.Write(sentence[x] + " ");
             }
+
         }
     }
 }
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,8 +1,5 @@
 ﻿using Algorithm;
-
 Console.WriteLine("Hello, to the algorithm and data structure problems");
 Console.WriteLine("Enter a sentence");
 String s = Console.ReadLine();
-Console.WriteLine("Enter the word you want to search");
-String search = Console.ReadLine();
-Algo.sortAndCall(s, search);
+Algo.insertionSort(s);
